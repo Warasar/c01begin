6ea74a5e471286a402302e0115260efa82a40b1a
Pavel <43359673+Warasar@users.noreply.github.com>
2020-05-19 16:38:18 +0900

    Add files via upload

---
+++ 1/12.cpp
@@ -0,0 +1,14 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	float g, r;
+	cin >> g;
+	r = (g / 180) * 3.14;
+	cout << fixed;
+	cout.precision(4);
+	cout << r << endl;
+
+}
---
+++ 2/2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	float r,h,sk,sc;
+	cin >> r >> h ;
+	sk = 3.14 * (r * r) * h;
+	sc = sk * 3;
+	cout << "obiem cilindra = " << sc << endl;
+	cout << "obiem konusa = " << sk << endl;
+}
